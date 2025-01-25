079c969542feef77798748b858541275ee573dcc
Zadig de Bem <zadigdebem@gmail.com>
2025-01-25 20:26:39 -0300

    fim primeiro modulo

---
+++ trabalhandoComArquivosC#/ByteBankIO/Program.cs
@@ -0,0 +1,40 @@
+﻿using ByteBankIO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024]; //1KB
+
+        numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+        
+        Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+
+        //foreach (var meuByte in buffer)
+        //{
+        //    Console.Write(meuByte);
+        //    Console.Write(" ");
+        //}
+    }
+}
