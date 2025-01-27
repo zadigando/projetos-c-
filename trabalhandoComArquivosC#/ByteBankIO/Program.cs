a28f19c32dd3a7bdbb6ed3e3d1fec4254c0b4edb
Zadig de Bem <zadigdebem@gmail.com>
2025-01-27 18:01:10 -0300

    começo modulo 3

--- trabalhandoComArquivosC#/ByteBankIO/Program.cs
+++ trabalhandoComArquivosC#/ByteBankIO/Program.cs
@@ -11,18 +11,40 @@ partial class Program
         {
             var leitor = new StreamReader(fluxoDeArquivo);
 
-            //var linha = leitor.ReadLine();
-
-            //var texto = leitor.ReadToEnd();
-
-            //var numero = leitor.Read(); //pega o primeiro byte
-
             while (!leitor.EndOfStream)
             {
                 var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                var msg = $"Conta número {contaCorrente.Numero}, " +
+                    $"agencia {contaCorrente.Agencia}, saldo {contaCorrente.Saldo}";
+                Console.WriteLine(msg);
             }
         }
         Console.ReadLine();
+    }
+
+    static ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+        // 375 4644 2483.13 Jonathan
+        var campos = linha.Split(',');
+
+        var agencia = campos[0];
+        var numero = campos[1];
+        var saldo = campos[2].Replace('.', ',');
+        var nomeTitular = campos[3];
+
+        var agenciaComInt = int.Parse(agencia);
+        var numeroComInt = int.Parse(numero);
+        var saldoComDouble = double.Parse(saldo);
+
+        var titular = new Cliente();
+        titular.Nome = nomeTitular;
+
+        var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
+        resultado.Depositar(saldoComDouble);
+        resultado.Titular = titular;
+
+        return resultado;
     }
 }
--- trabalhandoComArquivosC#/ByteBankIO/obj/Debug/net6.0/ByteBankIO.AssemblyInfo.cs
+++ trabalhandoComArquivosC#/ByteBankIO/obj/Debug/net6.0/ByteBankIO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ByteBankIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079c969542feef77798748b858541275ee573dcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a205b1a3b563a74c3c1ae7aad7f949424d4c425")]
 [assembly: System.Reflection.AssemblyProductAttribute("ByteBankIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ByteBankIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
